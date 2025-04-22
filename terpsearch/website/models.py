35e5f36ce0a95d701b146cc2042daff576cfcfb0
Aditya Sabu <asabu@terpmail.umd.edu>
2025-04-21 20:43:52 -0400

    Modify Flask's User() object

--- terpsearch/website/models.py
+++ terpsearch/website/models.py
@@ -1,9 +1,17 @@
-from . import db
+# from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
+
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(150), unique=True)
+#     password = db.Column(db.String(150))
+#     first_name = db.Column(db.String(150))
+
+class User(UserMixin):
+    def __init__(self, user_id, email, first_name, password_hash):
+        self.id = user_id
+        self.email = email
+        self.first_name = first_name
+        self.password_hash = password_hash
