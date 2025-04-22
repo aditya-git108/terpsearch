ac759d27451f7163856fc813a10d931cb9393d0d
Aditya Sabu <asabu@terpmail.umd.edu>
2025-04-21 20:48:28 -0400

    Create a constants class that will be called throughout code

--- terpsearch/constants/DynamoDbConstants.py
+++ terpsearch/constants/DynamoDbConstants.py
@@ -1,7 +1,15 @@
+import os
+
+
 class DynamoDbConstants:
     BSKY_POSTS_TABLE_NAME = 'BSKY_POSTS'
     BSKY_USERS_TABLE_NAME = 'BSKY_USERS'
-    DYNAMODB_URL = 'http://dynamodb-local:8000'
+    TERPSEARCH_LOGIN_TABLE_NAME = 'LOGIN'
+    DYNAMODB_REGION = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY_ID = os.getenv('AWS_SECRET_ACCESS_KEY')
+    FASTAPI_URL = os.getenv('FASTAPI_URL', 'http://localhost:8010')
+    DYNAMODB_URL = os.getenv('DYNAMODB_URL', 'http://localhost:8000')
     DYNAMODB_DEV_URL = 'http://localhost:8000'
-    FLASK_MODE = 'DEV'
+    DB_MODE = os.getenv('DB_MODE', 'DEV')
     FERNET_KEY = 'FERNET_KEY'
